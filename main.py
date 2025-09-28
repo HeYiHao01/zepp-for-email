70188d5315fe3f43550583ec9979b9ea25345837
HeYiHao01 <42639770+HeYiHao01@users.noreply.github.com>
2025-09-28 16:25:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ import requests
 sckey = 'NO'
 
 # 开启根据地区天气情况降低步数（默认关闭）
-open_get_weather = sys.argv[3]
+# open_get_weather = sys.argv[3]
+open_get_weather = 'False'
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = sys.argv[4]
+# area = sys.argv[4]
+area = "宁波"
 
 set_push = [True]
 
